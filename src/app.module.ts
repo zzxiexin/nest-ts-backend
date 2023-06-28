5fe482073aeb8f589c7901dfffdc2106a43f3aea
xinxin.yuan <xinxin.yuan@trip.com>
2023-06-29 02:13:20 +0800

    feat: 连接数据库

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
+const DatabaseConfig = TypeOrmModule.forRoot({
+  type: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  username: 'root',
+  password: 'xinxin',
+  database: 'mysql',
+  entities: [],
+  synchronize: true,
+});
 @Module({
-  imports: [],
+  imports: [UsersModule, DatabaseConfig],
   controllers: [AppController],
   providers: [AppService],
 })
