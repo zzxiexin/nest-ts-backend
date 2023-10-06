5be12633ef336df8ba0bf5e2dd840af1468b9ff0
bujianjia <bujianjia@pinduoduo.com>
2023-10-06 22:19:47 +0800

    feat: cros跨域设置

--- src/cors.ts
+++ src/cors.ts
@@ -1,21 +1,21 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
-import { Request, Response, NextFunction } from 'express';
+import { Response } from 'express';
 
 @Injectable()
 export class CorsMiddleware implements NestMiddleware {
-  use(req: Request, res: Response, next: NextFunction) {
+  use(req: any, res: Response, next: () => void) {
+    // 设置允许跨域的域名和请求头
     res.header('Access-Control-Allow-Origin', '*');
     res.header(
-      'Access-Control-Allow-Methods',
-      'GET, POST, PUT, DELETE, OPTIONS',
-    );
-    res.header(
       'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept',
-    );
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+    ); // 添加 Authorization
+
     if (req.method === 'OPTIONS') {
-      res.sendStatus(200);
+      // 如果是预检请求，直接返回成功状态
+      res.status(200).end();
     } else {
+      // 否则继续处理请求
       next();
     }
   }
